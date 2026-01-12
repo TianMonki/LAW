c3e9a5c28e1e6331e1f5dd049f3fbf24c6595dd4
Monki <83906746+TianMonki@users.noreply.github.com>
2026-01-12 15:37:47 +0800

    Update data_main.sh

--- data_code/data_main.sh
+++ data_code/data_main.sh
@@ -8,9 +8,9 @@ set -x
 # 定义Python脚本的名称，方便修改
 PYTHON_SCRIPT="code/data_main.py"
 
-# --- 任务1: 生成 0.5B token, 65k 长度, 含倒序 ---
+# --- 任务1 ---
 echo "=================================================="
-echo "开始任务 1: mixed_long_context (0.5B tokens, 65k len, with reverse)"
+echo "开始任务 1"
 echo "=================================================="
 python $PYTHON_SCRIPT \
     --input_file "data/process_code/final_aggregated_output.jsonl" \
@@ -19,43 +19,3 @@ python $PYTHON_SCRIPT \
     --token_limit_per_type 250000000 \
     --sequence_length 65536 \
     --use_reverse
-
-
-# --- 任务3: 1B token, 65k 长度, 不含倒序 ---
-echo "=================================================="
-echo "开始任务 3: data_wo_reverse (1B tokens, 65k len, without reverse)"
-echo "=================================================="
-python $PYTHON_SCRIPT \
-    --input_file "data/process_code/final_aggregated_output.jsonl" \
-    --output_file "data/process_code/data_wo_reverse.jsonl" \
-    --tokenizer_name "llm/Llama-2-7b-hf" \
-    --token_limit_per_type 500000000 \
-    --sequence_length 65536
-
-# --- 任务4: 2B token, 65k 长度, 含倒序 ---
-echo "=================================================="
-echo "开始任务 4: mixed_long_context_2B (2B tokens, 65k len, with reverse)"
-echo "=================================================="
-python $PYTHON_SCRIPT \
-    --input_file "data/process_code/final_aggregated_output.jsonl" \
-    --output_file "data/process_code/mixed_long_context_2B.jsonl" \
-    --tokenizer_name "llm/Llama-2-7b-hf" \
-    --token_limit_per_type 500000000 \
-    --sequence_length 65536 \
-    --use_reverse
-
-# --- 任务5: 1B token, 32k 长度, 含倒序 ---
-echo "=================================================="
-echo "开始任务 5: mixed_long_context_32k (1B tokens, 32k len, with reverse)"
-echo "=================================================="
-python $PYTHON_SCRIPT \
-    --input_file "data/process_code/final_aggregated_output.jsonl" \
-    --output_file "data/process_code/mixed_long_context_32k.jsonl" \
-    --tokenizer_name "llm/Llama-2-7b-hf" \
-    --token_limit_per_type 250000000 \
-    --sequence_length 32768 \
-    --use_reverse
-
-echo "=================================================="
-echo "所有任务已成功完成！"
-echo "=================================================="
